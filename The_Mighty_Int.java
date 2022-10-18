5a9a6032739a5d64ff6aff1c3529f059ba9a1aa9
karolbe12 <115897168+karolbe12@users.noreply.github.com>
2022-10-18 20:03:00 +0200

    Add file

---
+++ The_Mighty_Int.java
@@ -0,0 +1,15 @@
+package potegowanie_intow;
+
+
+public class The_Mighty_Int 
+{
+	public static int potegowanie (int podstawa, int potega)
+	{
+		int c = podstawa;
+		for (int i=1; i==potega; i++)
+		{
+			podstawa = podstawa*c;
+		}
+		return podstawa;
+	}
+}
